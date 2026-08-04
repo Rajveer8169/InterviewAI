4a334ee70b673a292ac0e1ecf58c4c0d1d0f2a9c
Rajveer8169 <rajveerrai098@gmail.com>
2026-08-04 22:50:32 +0530

    dashboard ui and api

--- BACKEND/src/controllers/interview.controller.js
+++ BACKEND/src/controllers/interview.controller.js
@@ -1,30 +1,66 @@
-import pdfParse from "pdf-parse"
-import generateInterviewReport from "../services/ai.service.js"
-import interviewReportModel from "../models/interviewReport.model.js"
-import interviewReportModel from "../models/interviewReport.model.js"
+import { PDFParse } from "pdf-parse";
+import generateInterviewReport from "../services/ai.service.js";
+import interviewReportModel from "../models/interviewReport.model.js";
 
+export const generateInterViewReportController = async (req, res) => {
+  const parser = new PDFParse({
+    data: Uint8Array.from(req.file.buffer),
+  });
 
-export const generateInterViewReportController = async(req,res) => {
+  const resumeContent = await parser.getText();
 
-    const resumeContent = await (new pdfParse.PDFParse(Uint8Array.from(req.file.buffer))).getText()
-    const {selfDescription,jobDescription} = req.body
+  const { selfDescription, jobDescription } = req.body;
 
-    const interviewReportByAi = await generateInterviewReport({
-        resume: resumeContent.text,
-        selfDescription,
-        jobDescription
-    })
+  const interviewReportByAi = await generateInterviewReport({
+    resume: resumeContent.text,
+    selfDescription,
+    jobDescription,
+  });
 
-    const interviewReport = await interviewReportModel.create({
-        user:req.user.id,
-        resume: resumeContent.text,
-        selfDescription,
-        jobDescription,
-        ...interViewReportByAi
-    })
+  const interviewReport = await interviewReportModel.create({
+    user: req.user.id,
+    resume: resumeContent.text,
+    selfDescription,
+    jobDescription,
+    ...interviewReportByAi,
+  });
+
+  res.status(200).json({
+    message: "Interview report generated successfully",
+    interviewReport,
+  });
+};
+
+export const getInterviewReportByIdController = async (req, res) => {
+  const { interviewId } = req.params;
+
+  const interviewReport = await interviewReportModel.findOne({
+    _id: interviewId,
+    user: req.user.id,
+  });
+
+  if (!interviewReport) {
+    return res.status(400).json({
+      message: "Interview report not found.",
+    });
+  }
+
+  res.status(200).json({
+    message: "Interview report fetched successfully",
+    interviewReport,
+  });
+};
+
+export const getAllInterviewReportsController = async (req, res) => {
+  const interviewReports = await interviewReportModel
+    .find({ user: req.user.id })
+    .sort({ createdAt: -1 })
+    .select(
+      "-resume -selfDescription -jobDescription -__v -updatedAt -technicalQuestions -behavioralQuestions -skillGaps -preparationPlan",
+    );
 
     res.status(200).json({
-        message:"Interview report generated successfully",
-        interviewReport
+        message:"Interview Reports fetched successfully",
+        interviewReports
     })
-}
+};
--- BACKEND/src/routes/interview.routes.js
+++ BACKEND/src/routes/interview.routes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { authUser } from "../middleware/auth.middleware.js";
-import { generateInterViewReportController } from "../controllers/interview.controller.js";
+import { generateInterViewReportController, getAllInterviewReportsController, getInterviewReportByIdController } from "../controllers/interview.controller.js";
 import upload from "../middleware/file.middleware.js";
 
 const interviewRouter = express.Router();
 
 interviewRouter.post("/",authUser,upload.single("resume"),generateInterViewReportController);
+interviewRouter.get("/report/:interviewId",authUser,getInterviewReportByIdController)
+interviewRouter.get("/",authUser,getAllInterviewReportsController)
 
 export default interviewRouter;
