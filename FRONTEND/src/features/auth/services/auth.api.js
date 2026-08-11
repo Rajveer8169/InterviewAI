ae4ffefc9eb415d55d032248d83202e2973c65fa
Rajveer8169 <rajveerrai098@gmail.com>
2026-08-11 19:00:38 +0530

    prepare frontend for deployment

--- FRONTEND/src/features/auth/services/auth.api.js
+++ FRONTEND/src/features/auth/services/auth.api.js
@@ -1,56 +1,63 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
-  withCredentials: true
+    baseURL: "https://interviewai-backend-a4o0.onrender.com",
+    withCredentials: true,
 });
 
 export const register = async ({ username, email, password }) => {
-  try {
-    const { data } = await api.post(
-      "/api/auth/register",
-      {
-        username,
-        email,
-        password,
-      }
-    );
-    alert(data.message);
-    return data;
-  } catch (err) {
-    console.log(err);
-  }
+    try {
+        const { data } = await api.post(
+            "/api/auth/register",
+            {
+                username,
+                email,
+                password,
+            }
+        );
+
+        alert(data.message);
+        return data;
+    } catch (err) {
+        console.log(err);
+    }
 };
 
 export const login = async ({ email, password }) => {
-  try {
-    const { data } = await api.post(
-      "/api/auth/login",
-      { email, password }
-    );
-    alert(data.message);
-    return data;
-  } catch (err) {
-    console.log(err);
-  }
+    try {
+        const { data } = await api.post(
+            "/api/auth/login",
+            {
+                email,
+                password,
+            }
+        );
+
+        alert(data.message);
+        return data;
+    } catch (err) {
+        console.log(err);
+    }
 };
 
 export const logout = async () => {
-  try {
-    const { data } = await api.post("/api/auth/logout");
-    alert(data.message);
-    return data;
-  } catch (err) {
-    console.log(err);
-  }
+    try {
+        const { data } = await api.post("/api/auth/logout");
+
+        alert(data.message);
+        return data;
+    } catch (err) {
+        console.log(err);
+    }
 };
 
 export const getMe = async () => {
-  try {
-    const { data } = await api.get("/api/auth/get-me");
-    console.log(data)
-    return data;
-  } catch (err) {
-    console.log(err);
-  }
+    try {
+        const { data } = await api.get("/api/auth/get-me");
+
+        console.log(data);
+        return data;
+    } catch (err) {
+        console.log(err);
+    }
 };
